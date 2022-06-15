7b182623c552e71cee7d7bcbf8cb7f62b0822b48
xolani <mcxolani@gmail.com>
2022-06-15 22:43:42 +0200

    add image logic

--- src/main/java/com/example/pokemon/pokemon/models/Pokemon.java
+++ src/main/java/com/example/pokemon/pokemon/models/Pokemon.java
@@ -3,13 +3,4 @@ package com.example.pokemon.pokemon.models;
 import java.util.ArrayList;
 
 public record Pokemon(String name, String url, Integer id, String image, ArrayList<Stat> stats) {
-
-    public String image() {
-        if (this.url == null) {
-            return String.format("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/%s.png", this.id.toString());
-        }
-
-        String id = this.url.split("/")[6];
-        return String.format("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%s.png", id);
-    }
 }
