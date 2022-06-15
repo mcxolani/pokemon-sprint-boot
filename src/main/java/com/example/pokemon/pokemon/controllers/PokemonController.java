0625f98bff5112e0c3fbe9d76555a40da918477c
xolani <mcxolani@gmail.com>
2022-06-15 22:48:05 +0200

    add controller

---
+++ src/main/java/com/example/pokemon/pokemon/controllers/PokemonController.java
@@ -0,0 +1,30 @@
+package com.example.pokemon.pokemon.controllers;
+
+import com.example.pokemon.pokemon.models.Pokemon;
+import com.example.pokemon.pokemon.models.PokemonList;
+import com.example.pokemon.pokemon.services.PokemonService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class PokemonController {
+
+    PokemonService pokemonService;
+
+    PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    @GetMapping("/")
+    public ArrayList<Pokemon> getAllPokemon() {
+        PokemonList pokemonResults = this.pokemonService.getAllPokemon();
+        return pokemonResults.results();
+    }
+
+    @GetMapping("/{name}")
+    public Pokemon getPokemon(@PathVariable("name") String name) {
+        return pokemonService.getPokemon(name);
+    }
+
+}
